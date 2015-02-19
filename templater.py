7a552161eab19d24b7b221635e51a915adff0166
Peter Gal <galpeter@inf.u-szeged.hu>
2015-02-19 17:36:31 +0100

    Use OptionParser instead of simple sys.argv.

--- templater.py
+++ templater.py
@@ -1,18 +1,28 @@
 #!/usr/bin/python
 import string
+import os
+
 
 if __name__ == "__main__":
-    import sys
+    from optparse import OptionParser
 
-    template_file = sys.argv[1]
+    parser = OptionParser()
+    parser.add_option("-t", "--template", dest="template_file",
+                      help="Input template file")
 
-    with open(template_file) as f:
+    (options, args) = parser.parse_args()
+
+    if not os.path.isfile(options.template_file):
+        sys.stderr.write("Invalid input template file")
+        exit(1)
+
+    with open(options.template_file) as f:
         data = f.read()
 
     template = string.Template(data)
     template_mapping = {}
 
-    for item in sys.argv[2:]:
+    for item in args:
         # item is in the following form:  KEY=VALUE
         print("-> Current replacer %s" % item)
         key, value = item.split("=", 1)
